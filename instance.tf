c17ff2b8f10c6954e81bab925ebbeb0eac954e10
shegoj <shegoj@yahoo.com>
2020-06-28 20:54:21 +0100

    all done

--- instance.tf
+++ instance.tf
@@ -6,8 +6,9 @@ resource "aws_instance" "test-instance" {
   ami           = "ami-0ea3405d2d2522162"
   instance_type = "t2.micro"
   key_name      = "june_devops"
+  
 
   tags = {
-    Name = "DevBox"
+    Name = "NewDevBox"
   }
 }
